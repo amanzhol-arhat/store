77f4a1a220c57fb053dec3e48e00212a215abb24
Arhat Amanzhol <aarhat144@gmail.com>
2025-04-11 23:05:22 +0500

    update views

--- store/users/views.py
+++ store/users/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render, HttpResponseRedirect
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
-from django.contrib import auth, messages
-from django.urls import reverse
+from django.contrib import auth
+from django.urls import reverse, reverse_lazy
 from products.models import Basket
-from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import CreateView, UpdateView
+from users.models import User
+
+
+class UserRegistrationView(CreateView):
+    model = User
+    form_class = UserRegistrationForm
+    template_name = 'users/registration.html'
+    success_url = reverse_lazy('users:login')
+    
+    def get_context_data(self, **kwargs):
+        context = super(UserRegistrationView, self).get_context_data(**kwargs)
+        context['title'] = 'Store - Регистрация'
+        return context 
 
 def login(request):
     if request.method == 'POST':
@@ -23,41 +36,58 @@ def login(request):
     context = {'form': form}
     return render(request, 'users/login.html', context)
 
-def registration(request):
-    if request.method == 'POST':
-        form = UserRegistrationForm(data=request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Поздравляем! Вы успешно зарегистрировались!')
-            return HttpResponseRedirect(reverse('users:login'))
-    else: 
-        form = UserRegistrationForm()
-    context = {'form' : form}
-    return render(request, 'users/registration.html', context)
-@login_required
-def profile(request):
-    if request.method == 'POST':
-        form = UserProfileForm(instance= request.user, data = request.POST, files=request.FILES)
-        if form.is_valid():
-            user = form.save(commit=False)
-            if 'image' in request.FILES:  # Проверяем, есть ли файл
-                user.image = request.FILES['image']  # Принудительно добавляем
-            user.save()
-            return HttpResponseRedirect(reverse('users:profile'))
-        else:
-            print(form.errors)
-    else:
-        form = UserProfileForm(instance=request.user)
-
-    context = {
-        'title': 'Store - Профиль', 
-        'form': form,
-        'baskets': Basket.objects.filter(user = request.user)
-        }
-    return render(request, 'users/profile.html', context)
+class UserProfileUpdateView(UpdateView):
+    model = User
+    template_name = 'users/profile.html'
+    form_class = UserProfileForm
+    
+    def get_success_url(self):
+        return reverse_lazy('users:profile', args=(self.object.id, ))
+    
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        context['title'] = 'Store - Личный кабинет'
+        context['baskets'] = Basket.objects.filter(user = self.object)
+        return context
 
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('index'))
 
 
+    
+
+
+
+# def registration(request):
+#     if request.method == 'POST':
+#         form = UserRegistrationForm(data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, 'Поздравляем! Вы успешно зарегистрировались!')
+#             return HttpResponseRedirect(reverse('users:login'))
+#     else: 
+#         form = UserRegistrationForm()
+#     context = {'form' : form}
+#     return render(request, 'users/registration.html', context)
+
+# def profile(request):
+#     if request.method == 'POST':
+#         form = UserProfileForm(instance= request.user, data = request.POST, files=request.FILES)
+#         if form.is_valid():
+#             user = form.save(commit=False)
+#             if 'image' in request.FILES:  # Проверяем, есть ли файл
+#                 user.image = request.FILES['image']  # Принудительно добавляем
+#             user.save()
+#             return HttpResponseRedirect(reverse('users:profile'))
+#         else:
+#             print(form.errors)
+#     else:
+#         form = UserProfileForm(instance=request.user)
+
+#     context = {
+#         'title': 'Store - Профиль', 
+#         'form': form,
+#         'baskets': Basket.objects.filter(user = request.user)
+#         }
+#     return render(request, 'users/profile.html', context)
