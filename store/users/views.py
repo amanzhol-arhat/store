0d6800c1503b44d992bf2c19edd9594c633af802
Arhat Amanzhol <aarhat144@gmail.com>
2025-04-12 00:10:10 +0500

    Create own Mixin and enter this in project

--- store/users/views.py
+++ store/users/views.py
@@ -1,38 +1,40 @@
-from django.shortcuts import render, HttpResponseRedirect
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
-from django.contrib import auth
-from django.urls import reverse_lazy, reverse
+from django.urls import reverse_lazy
 from products.models import Basket
 from django.views.generic.edit import CreateView, UpdateView
 from users.models import User
 from django.contrib.auth.views import LoginView
+from django.contrib.messages.views import SuccessMessageMixin
+from common.views import TitleMixin
 
-class UserRegistrationView(CreateView):
+class UserRegistrationView(TitleMixin, SuccessMessageMixin, CreateView):
     model = User
     form_class = UserRegistrationForm
     template_name = 'users/registration.html'
     success_url = reverse_lazy('users:login')
+    success_message = 'Поздравляем! Вы успешно зарегистрировались!'
+    title = 'Store - Регистрация'
     
     def get_context_data(self, **kwargs):
         context = super(UserRegistrationView, self).get_context_data(**kwargs)
-        context['title'] = 'Store - Регистрация'
         return context 
 
-class UserLoginView(LoginView):
+class UserLoginView(TitleMixin, LoginView):
     template_name = 'users/login.html'
     form_class = UserLoginForm
-
-class UserProfileView(UpdateView):
+    title = 'Store - Авторизация'
+    
+class UserProfileView(TitleMixin, UpdateView):
     model = User
     template_name = 'users/profile.html'
     form_class = UserProfileForm
+    title = 'Store - личный кабинет'
     
     def get_success_url(self):
         return reverse_lazy('users:profile', args=(self.object.id, ))
     
     def get_context_data(self, **kwargs):
         context =  super().get_context_data(**kwargs)
-        context['title'] = 'Store - Личный кабинет'
         context['baskets'] = Basket.objects.filter(user = self.object)
         return context
 
