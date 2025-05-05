cf4f2813a89069fab3d36be872eb106b439fe1dc
Arhat Amanzhol <aarhat144@gmail.com>
2025-05-05 23:16:47 +0500

    connect psql

--- store/store/settings.py
+++ store/store/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'store_db',
+        'USER': 'store_username',
+        'PASSWORD': 'store_password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
